dccd8e93532ec0d19ad9e60f28c7d452f3e48ce1
Pramuditha Vidushan Assadduma <75202685+eXtizi@users.noreply.github.com>
2025-04-03 02:57:00 +0530

    Updated pagination.tsx: PaginationPrevious,

---
+++ components/ui/pagination.tsx
@@ -0,0 +1,5 @@
+const Pagination = ({ className, ...props }: React.ComponentProps<"nav">) => (
+Pagination.displayName = "Pagination"
+  <li ref={ref} className={cn("", className)} {...props} />
+  >
+  PaginationPrevious,
