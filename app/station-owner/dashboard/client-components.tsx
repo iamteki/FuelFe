246fbc990450d2ec9f7c55e08beba9e0d4fab33e
Pramuditha Vidushan Assadduma <75202685+eXtizi@users.noreply.github.com>
2025-04-08 03:59:34 +0530

    Refactored client-components.tsx: <Progress

--- app/station-owner/dashboard/client-components.tsx
+++ app/station-owner/dashboard/client-components.tsx
@@ -1,3 +1,7 @@
             <p className="font-medium">ABC-1234</p>
             <p className="text-sm text-slate-500">May 18, 2025 â€¢ 10:23 AM</p>
           </div>
+            <Progress 
+              value={15}
+              className="h-2"
+              indicatorClassName="bg-red-500"
