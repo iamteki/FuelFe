4aa69af9db7e4727012728c4075a4a23ff72b71d
Pramuditha Vidushan Assadduma <75202685+eXtizi@users.noreply.github.com>
2025-04-12 15:56:21 +0530

    Refactored use-toast.ts: id: string

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -1,2 +1,5 @@
       // ! Side effects ! - This could be extracted into a dismissToast() action,
       // but I'll keep it here for simplicity
+  id: string
+  title?: React.ReactNode
+  description?: React.ReactNode
