b5747941a6fb44626451a7e7f494782401cdf5ae
Pramuditha Vidushan Assadduma <75202685+eXtizi@users.noreply.github.com>
2025-04-14 09:02:13 +0530

    Added layout.tsx: title: "Fuel Quota Management System",

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,3 +2,5 @@ export const metadata: Metadata = {
   title: "Fuel Quota Management System",
   description: "National Fuel Crisis Management Solution",
 }
+  title: "Fuel Quota Management System",
+  description: "National Fuel Crisis Management Solution",
