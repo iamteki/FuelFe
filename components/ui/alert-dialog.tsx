2c48ab698e494b6a09d90bd90b7a2fffed412c0c
Pramuditha Vidushan Assadduma <75202685+eXtizi@users.noreply.github.com>
2025-04-16 10:28:10 +0530

    Tweaked alert-dialog.tsx: AlertDialogTrigger,

--- components/ui/alert-dialog.tsx
+++ components/ui/alert-dialog.tsx
@@ -5,3 +5,8 @@
 const AlertDialogTrigger = AlertDialogPrimitive.Trigger
 
 const AlertDialogPortal = AlertDialogPrimitive.Portal
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogFooter,
+  AlertDialogTitle,
