80779069ab4774a52dfab75d276488a9bd7426e9
Pramuditha Vidushan Assadduma <75202685+eXtizi@users.noreply.github.com>
2025-04-16 09:34:43 +0530

    Tweaked calendar.tsx: day_range_middle:

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -2,3 +2,6 @@
   classNames,
   showOutsideDays = true,
   ...props
+        day_range_middle:
+          "aria-selected:bg-accent aria-selected:text-accent-foreground",
+        day_hidden: "invisible",
