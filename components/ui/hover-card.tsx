2e5c298c57a5886db6e4d9a886d7c7f34def9769
Pramuditha Vidushan Assadduma <75202685+eXtizi@users.noreply.github.com>
2025-04-17 05:03:38 +0530

    Updated hover-card.tsx: />

--- components/ui/hover-card.tsx
+++ components/ui/hover-card.tsx
@@ -1,4 +1,8 @@
 "use client"
+
+import { cn } from "@/lib/utils"
 const HoverCard = HoverCardPrimitive.Root
+  React.ComponentPropsWithoutRef<typeof HoverCardPrimitive.Content>
+  <HoverCardPrimitive.Content
     sideOffset={sideOffset}
   />
