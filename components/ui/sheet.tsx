e94a5466158c82a76ff4f6c9cae9fa95225012f9
Pramuditha Vidushan Assadduma <75202685+eXtizi@users.noreply.github.com>
2025-04-20 18:38:15 +0530

    Refactored sheet.tsx: <SheetPrimitive.Overlay

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -1,2 +1,5 @@
   <SheetPortal>
     <SheetOverlay />
+  <SheetPrimitive.Overlay
+    className={cn(
+      "fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
