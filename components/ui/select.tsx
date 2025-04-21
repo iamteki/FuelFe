c4e2d9593d42fac7fa8d95bda71acb6a365b2737
Pramuditha Vidushan Assadduma <75202685+eXtizi@users.noreply.github.com>
2025-04-22 01:21:21 +0530

    Updated select.tsx: <SelectPrimitive.ScrollDownButton

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -1,2 +1,6 @@
   React.ComponentPropsWithoutRef<typeof SelectPrimitive.ScrollUpButton>
 >(({ className, ...props }, ref) => (
+  <SelectPrimitive.ScrollDownButton
+    ref={ref}
+    className={cn(
+      "flex cursor-default items-center justify-center py-1",
