486b287a46bf905e43c912e37ce0cf0a9d8346b8
Pramuditha Vidushan Assadduma <75202685+eXtizi@users.noreply.github.com>
2025-04-26 02:45:55 +0530

    Refactored checkbox.tsx: export { Checkbox }

--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -1,3 +1,8 @@
+import * as React from "react"
+>(({ className, ...props }, ref) => (
+    className={cn(
+    {...props}
     <CheckboxPrimitive.Indicator
+    </CheckboxPrimitive.Indicator>
 
 export { Checkbox }
