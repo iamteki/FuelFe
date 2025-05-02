6e18301b8d2d93aa45460524ae7b824ed4d4983f
Pramuditha Vidushan Assadduma <75202685+eXtizi@users.noreply.github.com>
2025-05-02 18:58:59 +0530

    Added auth-form.tsx: </form>

--- components/auth/auth-form.tsx
+++ components/auth/auth-form.tsx
@@ -1,4 +1,4 @@
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input placeholder="email@example.com" {...field} />
-                  </FormControl>
+
+    try {
+        title: "Registration successful",
+          </form>
