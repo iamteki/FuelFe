2c054c66f422c593f527e2cef60ef167598aee49
Pramuditha Vidushan Assadduma <75202685+eXtizi@users.noreply.github.com>
2025-05-10 10:40:51 +0530

    Refactored popover.tsx: PopoverContent.displayName = PopoverPrimitive.Content.displayName

--- components/ui/popover.tsx
+++ components/ui/popover.tsx
@@ -1,5 +1,10 @@
 import * as React from "react"
+
 const PopoverTrigger = PopoverPrimitive.Trigger
 
+  React.ElementRef<typeof PopoverPrimitive.Content>,
+>(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
+  <PopoverPrimitive.Portal>
       ref={ref}
+      sideOffset={sideOffset}
 PopoverContent.displayName = PopoverPrimitive.Content.displayName
