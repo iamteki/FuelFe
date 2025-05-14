98c15a4f05fd2cbf388d624d00ad7b071978ad2d
Pramuditha Vidushan Assadduma <75202685+eXtizi@users.noreply.github.com>
2025-05-14 23:06:04 +0530

    Updated chart.tsx: {payload.map((item) => {

--- components/ui/chart.tsx
+++ components/ui/chart.tsx
@@ -1,3 +1,7 @@
         return null
       }
 
+        {payload.map((item) => {
+          const key = `${nameKey || item.dataKey || "value"}`
+          const itemConfig = getPayloadConfigFromPayload(config, item, key)
+
