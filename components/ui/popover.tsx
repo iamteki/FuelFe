f5f23d9fc57ef5a872c7447dd2e36a769e7fee07
Pramuditha Vidushan Assadduma <75202685+eXtizi@users.noreply.github.com>
2025-05-15 11:45:29 +0530

    Updated popover.tsx: import { cn } from "@/lib/utils"

--- components/ui/popover.tsx
+++ components/ui/popover.tsx
@@ -2,3 +2,8 @@
 ))
 PopoverContent.displayName = PopoverPrimitive.Content.displayName
 
+import { cn } from "@/lib/utils"
+
+const Popover = PopoverPrimitive.Root
+
+const PopoverTrigger = PopoverPrimitive.Trigger
