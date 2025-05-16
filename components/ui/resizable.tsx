10ceacfe6b3ebfb6597ac8ede5baed29ecef9305
Pramuditha Vidushan Assadduma <75202685+eXtizi@users.noreply.github.com>
2025-05-17 02:45:18 +0530

    Updated resizable.tsx: className={cn(

--- components/ui/resizable.tsx
+++ components/ui/resizable.tsx
@@ -8,3 +8,7 @@ import * as ResizablePrimitive from "react-resizable-panels"
 
 import { cn } from "@/lib/utils"
 
+    className={cn(
+      "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
+      className
+    )}
