0f404de398e6665cb062c43e70aae267bfb206d1
Pramuditha Vidushan Assadduma <75202685+eXtizi@users.noreply.github.com>
2025-05-21 21:23:58 +0530

    Added navigation-menu.tsx: const NavigationMenuLink = NavigationMenuPrimitive.Link

--- components/ui/navigation-menu.tsx
+++ components/ui/navigation-menu.tsx
@@ -1,2 +1,4 @@
-    {children}
-    <NavigationMenuViewport />
+  <NavigationMenuPrimitive.List
+>(({ className, children, ...props }, ref) => (
+NavigationMenuTrigger.displayName = NavigationMenuPrimitive.Trigger.displayName
+const NavigationMenuLink = NavigationMenuPrimitive.Link
