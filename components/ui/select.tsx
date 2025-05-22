25393552da8bdd3bc13fcbf85d1368aa6e6d3e5f
Pramuditha Vidushan Assadduma <75202685+eXtizi@users.noreply.github.com>
2025-05-22 23:34:54 +0530

    Tweaked select.tsx: className={cn(

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -1,1 +1,6 @@
+
+  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>
   React.ComponentPropsWithoutRef<typeof SelectPrimitive.ScrollUpButton>
+          "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
+  <SelectPrimitive.Label
+    className={cn(
