a47c60dc1a5d3e75826df4143683fa7954a42eab
Pramuditha Vidushan Assadduma <75202685+eXtizi@users.noreply.github.com>
2025-05-22 18:00:45 +0530

    Added next.config.mjs: export default nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
+/** @type {import('next').NextConfig} */
   },
 }
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
+export default nextConfig
