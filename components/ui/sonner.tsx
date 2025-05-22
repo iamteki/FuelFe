a1a1a6f4b56d4958d25d7eb4e6e8503166256d66
Pramuditha Vidushan Assadduma <75202685+eXtizi@users.noreply.github.com>
2025-05-22 23:49:52 +0530

    Added sonner.tsx:

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -2,3 +2,6 @@ import { Toaster as Sonner } from "sonner"
 
 type ToasterProps = React.ComponentProps<typeof Sonner>
 
+
+import { useTheme } from "next-themes"
+import { Toaster as Sonner } from "sonner"
